a064c262f951a204d48e6cf4a8fe650719827c38
Pablo Braulio <brulics@gmail.com>
2022-07-30 18:20:17 +0200

    Fix read request

--- pkg/common/httpx/request.go
+++ pkg/common/httpx/request.go
@@ -6,8 +6,7 @@ import (
 	"net/http"
 )
 
-//readRequest Return error if request is invalid
-func readRequest(w http.ResponseWriter, r *http.Request, req interface{}) error {
+func ReadRequest(w http.ResponseWriter, r *http.Request, req interface{}) error {
 	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
 		code, errs := buildInvalidRequestErrorsResponse(fmt.Sprintf("invalid request: %s", err.Error()))
 		WriteErrorResponse(w, code, errs...)
