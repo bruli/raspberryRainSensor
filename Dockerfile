0ba03f52d889e704de333a6da3ee3f0cfa025dd5
Pablo Braulio <brulics@gmail.com>
2022-08-07 16:53:22 +0200

    Add time type

--- Dockerfile
+++ Dockerfile
@@ -5,6 +5,6 @@ WORKDIR /app
 
 RUN apt-get update && apt-get dist-upgrade -y
 RUN apt-get install -y make gcc gcc-arm-linux-gnueabi
-COPY --from=golang:1.17.9-bullseye /usr/local/go/ /usr/local/go/
+COPY --from=golang:1.17.13-bullseye /usr/local/go/ /usr/local/go/
 RUN echo "export PATH=\$PATH:/usr/local/go/bin" >> ~/.bashrc
 #RUN go version
---
+++ pkg/common/vo/time.go
@@ -0,0 +1,27 @@
+package vo
+
+import (
+	"errors"
+	"strconv"
+	"time"
+)
+
+var ErrInvalidZeroTime = errors.New("invalid zero time to build Time object")
+
+type Time time.Time
+
+func ParseFromTime(t time.Time) (Time, error) {
+	if t.IsZero() {
+		return Time{}, ErrInvalidZeroTime
+	}
+	return Time(t), nil
+}
+
+func (t Time) EpochString() string {
+	unix := time.Time(t).Unix()
+	return strconv.Itoa(int(unix))
+}
+
+func TimeNow() Time {
+	return Time(time.Now())
+}
