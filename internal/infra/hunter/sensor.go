df9f8ae8956a83ff3311c48ababd1c7a48bd0e37
Pablo Braulio <brulics@gmail.com>
2023-04-24 15:14:21 +0200

    Fix restart service on deploy. Fix hunter sensor reference.

--- internal/infra/hunter/sensor.go
+++ internal/infra/hunter/sensor.go
@@ -7,32 +7,37 @@ import (
 	"github.com/stianeikeland/go-rpio/v4"
 )
 
-const RainReference = 1000
+const RainReference = 600
 
 type RainSensor struct{}
 
-func (r RainSensor) Read(_ context.Context) (rain.Rain, error) {
-	if err := rpio.Open(); err != nil {
-		return rain.Rain{}, err
+func (r RainSensor) Read(ctx context.Context) (rain.Rain, error) {
+	select {
+	case <-ctx.Done():
+		return rain.Rain{}, ctx.Err()
+	default:
+		if err := rpio.Open(); err != nil {
+			return rain.Rain{}, err
+		}
+
+		defer func() {
+			_ = rpio.Close()
+		}()
+
+		if err := rpio.SpiBegin(rpio.Spi0); err != nil {
+			return rain.Rain{}, err
+		}
+
+		rpio.SpiSpeed(1000000)
+		rpio.SpiChipSelect(0)
+		channel := byte(0)
+		data := []byte{1, (8 + channel) << 4, 0}
+
+		rpio.SpiExchange(data)
+
+		value := int(data[1]&3)<<8 + int(data[2])
+		defer rpio.SpiEnd(rpio.Spi0)
+
+		return rain.New(value, value > RainReference), nil
 	}
-
-	defer func() {
-		_ = rpio.Close()
-	}()
-
-	if err := rpio.SpiBegin(rpio.Spi0); err != nil {
-		return rain.Rain{}, err
-	}
-
-	rpio.SpiSpeed(1000000)
-	rpio.SpiChipSelect(0)
-	channel := byte(0)
-	data := []byte{1, (8 + channel) << 4, 0}
-
-	rpio.SpiExchange(data)
-
-	value := int(data[1]&3)<<8 + int(data[2])
-	defer rpio.SpiEnd(rpio.Spi0)
-
-	return rain.New(value, value > RainReference), nil
 }
